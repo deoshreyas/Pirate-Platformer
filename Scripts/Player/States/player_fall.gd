aad0ad5fd18290c6d4a35e8d202adef011d5f9bc
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-06-26 19:05:31 +0530

    Fix bugs, Experiment with Coyote Timing, Experiment with Parallax for Background layer

--- Scripts/Player/States/player_fall.gd
+++ Scripts/Player/States/player_fall.gd
@@ -1,12 +1,20 @@
 extends PlayerBaseState
 
+@onready var coyote_timer = $CoyoteTimer
+
 func enter():
 	play("fall")
+	if fsm.previous_state!="jump":
+		coyote_timer.start()
 	
 func physics_update(delta):
 	move(delta, true)
-	if object.is_on_floor():
-		if input.x==0:
+	if not coyote_timer.is_stopped() && input.jump_just_pressed:
+		change_state("jump")
+	elif object.is_on_floor():
+		if input.jump_buffer:
+			change_state("jump")
+		elif input.x==0:
 			change_state("idle")
 		else:
 			change_state("run")
--- Scripts/Player/States/player_jump.gd
+++ Scripts/Player/States/player_jump.gd
@@ -5,7 +5,9 @@ var variable_jump_height
 func enter():
 	play("idle")
 	object.velocity.y = -400
+	object.velocity.x = input.x * Player.MAX_SPEED
 	variable_jump_height = false 
+	input.jump_buffer = false 
 	
 func physics_update(delta):
 	move(delta, true)
